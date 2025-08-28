36a63cb41895f3c4d87b8979cce08dfce5240cbc
developervikas-maker <sv4987337@gmail.com>
2025-08-28 09:26:43 +0530

    Add files via upload

---
+++ q12.py
@@ -0,0 +1,9 @@
+def pattren(n=5):
+    for i in range(n):
+        left = '*' * i
+        spaces = ' ' * (2 * (n - i))
+        right ="*"*i
+        print(left+spaces+right)
+    print("*"*(2*n))
+pattren()
+
---
+++ q8.py
@@ -0,0 +1,8 @@
+for i in range(4):
+    for j in range(5):
+        if j==0 or j==4:
+            print("*" ,end="")
+        else:
+            print("   ",end="")
+    print()
+
